4f454caed638b7c68e152447cf0eef25a0fedee9
arnoldxu <u6065914@anu.edu.au>
2024-09-15 07:44:41 +1000

    aaaaa

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c4e68f9ef9d6fbe4500dc46140d53d05da930d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7e5af26e5b92c14427ef22cceddbc49e632613f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
